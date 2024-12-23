7ba447269abce9a911b52e279ce055268b1b1b5b
Mmesomadavid <chukwunoyelummesoma1@gmail.com>
2024-12-23 21:16:40 +0100

    added new changes

--- server.js
+++ server.js
@@ -1,72 +1,71 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+
 import express from 'express';
 import mongoose from 'mongoose';
 import http from "http";
-import errorMiddleware from "./middleware/errorMiddleware.js";
+import errorMiddleware from "./middleware/errroMiddleware.js";
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
+
 // routes
-import authRoutes from "./routes/authRoutes.js";
-import doctorRoutes from "./routes/doctorRoutes.js";
-import patientRoutes from './routes/patientRoutes.js';
+import authRoutes from "./routes/authRoutes.js"
+import doctorRoutes from "./routes/doctorRoutes.js"
+import patientRoutes from './routes/patientRoutes.js'
 import { verifyTransporter } from './services/emailConfig.js';
 
 // Create Express app
 const app = express();
 const server = http.createServer(app);
 
-// Apply body parser middleware
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-// Verify email transporter
 verifyTransporter();
 
-// Check for required environment variables
+// checking for required ENV
 if (!process.env.JWT_SECRET_KEY) {
-  console.log("Fatal Error: JWT_SECRET_KEY is required");
-  process.exit(1);
-}
+    console.log("Fatal Error: jwtPrivateKey is required");
+    process.exit(1);
+  }
 
-// Connect to MongoDB
+
+// connecting to MongoDB
 mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log("Connected to MongoDB..."))
   .catch((e) => console.log("Error connecting to MongoDB", e));
 
-// CORS middleware (place this at the top)
-const allowedOrigins = ['https://primehealth-three.vercel.app']; // Your frontend domain
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('CORS not allowed'), false);
-    }
-  },
-  methods: ['GET', 'POST'],
-}));
+// CORS middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 
 // Middleware
+app.use(cors());
 app.use(express.json());
+
+
 
 // Routes
 app.use('/api/auth', authRoutes); // Authentication routes (signup, login, OTP verification)
 app.use('/api/doctor', doctorRoutes); 
 app.use('/api/patient', patientRoutes); // Authentication routes (signup, login, OTP verification)
 
+
 // Error handling
 app.use(errorMiddleware);
 
-// Listening to port
-const PORT = process.env.PORT || 5000;
+
+// listening to port
+const PORT = !process.env.PORT ? 5000 : process.env.PORT;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-// Root route
 app.get("/", (req, res) => {
-  res.send("Yooo! API 💨💨💨 ");
+  res.header("Access-Control-Allow-Origin", "*").send("Yooo! API 💨💨💨 ");
 });
